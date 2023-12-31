b582558f320904ea3df5a1ed9f85a364b8f5483b
Hüseyin Sürer <huseyin.surer@student.nbi-handelsakademin.se>
2023-12-31 18:54:54 +0100

    Skapat valideringar för cat, dogs och users

---
+++ Application/Validators/Dog/DogValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using Application.Dtos;
+
+namespace Application.Validators.Dog
+{
+    public class DogValidator : AbstractValidator<DogDto>
+    {
+        public DogValidator()
+        {
+            RuleFor(dog => dog.Name)
+                .NotEmpty().WithMessage("Dog Name cannot be empty.")
+                .NotNull().WithMessage("Dog Name cannot be null.");
+
+            RuleFor(dog => dog.Breed)
+                .NotEmpty().WithMessage("Dog Breed cannot be empty.")
+                .NotNull().WithMessage("Dog Breed cannot be null.");
+
+            RuleFor(dog => dog.Weight)
+                .NotNull().WithMessage("Weight must be specified.")
+                .GreaterThan(0).WithMessage("Weight must be greater than 0.");
+        }
+    }
+}
